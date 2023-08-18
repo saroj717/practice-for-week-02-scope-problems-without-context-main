b34a6bafb74ba35bb66da2cb1f87919807585ac1
Saroj Shrestha <sarojshrestha@Sarojs-iMac.local>
2023-08-17 20:26:32 -0600

    first commit

---
+++ problems/05-arrow-mirror-array.js
@@ -0,0 +1,22 @@
+/***********************************************************************
+Write a function `mirrorArray(array)` that takes in an array as an
+argument and returns a new array "mirrored" as shown in the examples:
+
+Examples:
+
+mirrorArray([1,2,3]); // => [ 1, 2, 3, 3, 2, 1 ]
+mirrorArray(['a', 'b', 'c', 'd']); // => [ 'a', 'b', 'c', 'd', 'd', 'c', 'b', 'a' ]
+***********************************************************************/
+
+const mirrorArray = (array) => {
+  let cpArray = array.slice();
+  let newArray = [];
+  let reverseArray = cpArray.reverse();
+  return newArray = array.concat(reverseArray);
+};
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+try {
+  module.exports = mirrorArray;
+} catch (e) {
+  return null;
+}
